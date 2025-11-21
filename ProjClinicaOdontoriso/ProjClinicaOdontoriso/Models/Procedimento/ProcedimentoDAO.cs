ce853f41402428f05475c6589e242afd6cac0393
ketlysff <ketlyfreire21@gmail.com>
2025-11-20 22:12:27 -0400

    commit Editar Procedimento
    Adicionei a função pra editar procedimento, mas não esta salvando.

--- ProjClinicaOdontoriso/ProjClinicaOdontoriso/Models/Procedimento/ProcedimentoDAO.cs
+++ ProjClinicaOdontoriso/ProjClinicaOdontoriso/Models/Procedimento/ProcedimentoDAO.cs
@@ -1,4 +1,5 @@
 ﻿using ProjClinicaOdontoriso.Configs;
+using ProjClinicaOdontoriso.Models.Paciente;
 using System.Data;
 
 namespace ProjClinicaOdontoriso.Models
@@ -53,6 +54,51 @@ namespace ProjClinicaOdontoriso.Models
             }
 
             return lista;
+        }
+        public Procedimento? BuscarPorId(int id)
+        {
+            var comando = _conexao.CreateCommand(
+                "SELECT * FROM procedimento WHERE id_pro = @id;");
+            comando.Parameters.AddWithValue("@id", id);
+
+            var leitor = comando.ExecuteReader();
+
+            if (leitor.Read())
+            {
+                var procedimento = new Procedimento();
+                procedimento.Id = leitor.GetInt32("id_pro");
+                procedimento.Nome = leitor.IsDBNull(leitor.GetOrdinal("nome_pro")) ? "" : leitor.GetString("nome_pro");
+                procedimento.Tempo = leitor.IsDBNull(leitor.GetOrdinal("tempo_pro")) ? DateTime.Today : DateTime.Today + leitor.GetTimeSpan("tempo_pro");
+                procedimento.Descricao = leitor.IsDBNull(leitor.GetOrdinal("descricao_pro")) ? "" : leitor.GetString("descricao_pro");
+                procedimento.Valor = leitor.IsDBNull(leitor.GetOrdinal("valor_pro")) ? 0 : leitor.GetFloat("valor_pro");
+
+                return procedimento;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Atualizar(Procedimento procedimento)
+        {
+            try
+            {
+                var comando = _conexao.CreateCommand(
+                    "UPDATE procedimento SET nome_pro = @_procedimento, tempo_pro = @_tempo, descricao_pro = @_descricao, valor_pro = @_valor WHERE id_pro = @_id;");
+
+                comando.Parameters.AddWithValue("@_nome", procedimento.Nome);
+                comando.Parameters.AddWithValue("@_tempo", procedimento.Tempo);
+                comando.Parameters.AddWithValue("@_descricao", procedimento.Descricao);
+                comando.Parameters.AddWithValue("@_valor", procedimento.Valor);
+                comando.Parameters.AddWithValue("@_id", procedimento.Id);
+
+                comando.ExecuteNonQuery();
+            }
+            catch
+            {
+                throw;
+            }
         }
     }
 }
